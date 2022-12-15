e42bcb9c6d072f6ab8972f578adf21a35820df97
ozgrserkan <ozgr.serkan09@gmail.com>
2022-12-15 15:38:34 +0300

    register page added

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>ÖZGÜR SOCIAL</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci,
+            sit. Velit unde maiores assumenda, ex ipsam libero enim placeat
+          </p>
+          <span>Do you have an account</span>
+          <button className="register">Register</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
