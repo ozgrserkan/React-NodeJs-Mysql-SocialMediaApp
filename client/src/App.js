25979f534d710bbd5af47d9254108ea3ea539f67
ozgrserkan <ozgr.serkan09@gmail.com>
2022-12-16 12:24:19 +0300

    dark mode added

--- client/src/App.js
+++ client/src/App.js
@@ -11,12 +11,13 @@ import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Navbar from "./components/navbar/Navbar";
+import "./style.scss";
 function App() {
   const currentUser = true;
 
   const Layout = () => {
     return (
-      <div>
+      <div className="theme-dark">
         <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
